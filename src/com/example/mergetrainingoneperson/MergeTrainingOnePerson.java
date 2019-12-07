0c6372840a8e6bf807a0c308b88c60611d0580dd
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-07 13:31:35 +0100

    Added one method

--- src/com/example/mergetrainingoneperson/MergeTrainingOnePerson.java
+++ src/com/example/mergetrainingoneperson/MergeTrainingOnePerson.java
@@ -1,4 +1,16 @@
 package com.example.mergetrainingoneperson;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MergeTrainingOnePerson {
+
+    private String name;
+
+    public MergeTrainingOnePerson(String name) {
+        this.name = name;
+    }
+
+    public void getName() {
+        this.name = name;
+    }
 }
