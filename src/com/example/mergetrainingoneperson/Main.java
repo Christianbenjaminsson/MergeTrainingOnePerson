f3a2c0eb75bf516a292c90df6c4cdc9a63ce9c20
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-07 13:16:43 +0100

    Created two classes and push to master

---
+++ src/com/example/mergetrainingoneperson/Main.java
@@ -0,0 +1,9 @@
+package com.example.mergetrainingoneperson;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("This is a test working with branches and merge to master");
+    }
+}
---
+++ src/com/example/mergetrainingoneperson/MergeTrainingOnePerson.java
@@ -0,0 +1,4 @@
+package com.example.mergetrainingoneperson;
+
+public class MergeTrainingOnePerson {
+}
