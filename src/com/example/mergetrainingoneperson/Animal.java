24729bd67ba2be27ca177a210ad93a25e35a5cbc
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-11 14:07:44 +0100

    Created class Animal

---
+++ src/com/example/mergetrainingoneperson/Animal.java
@@ -0,0 +1,14 @@
+package com.example.mergetrainingoneperson;
+
+public class Animal {
+
+    public String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
